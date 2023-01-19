800595db1e8c64891c52cc51aa3f7115d64da0b0
Relentless <relentless@rlnt.dev>
2023-01-19 20:18:45 +0100

    AlmostManager docs

--- Common/src/main/java/com/almostreliable/almostlib/AlmostManager.java
+++ Common/src/main/java/com/almostreliable/almostlib/AlmostManager.java
@@ -7,9 +7,10 @@ import com.almostreliable.almostlib.registry.BlockRegistration;
 import com.almostreliable.almostlib.registry.ItemRegistration;
 import com.almostreliable.almostlib.registry.Registration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
 
 /**
- * Simple manager which holds registrations for a mod.
+ * Simple manager to hold registrations for a specified namespace.
  */
 public class AlmostManager {
 
@@ -27,40 +28,91 @@ public class AlmostManager {
         this.blockEntities = Registration.blockEntities(namespace);
     }
 
+    /**
+     * Creates a new AlmostManager for the given namespace.
+     *
+     * @param namespace The namespace to use for this manager.
+     * @return A new AlmostManager.
+     */
     public static AlmostManager create(String namespace) {
         return new AlmostManager(namespace);
     }
 
+    /**
+     * Sets the default creative tab for all items registered by this manager.
+     * <p>
+     * Can be overwritten per item by calling {@link ItemRegistration.Builder#creativeTab(CreativeModeTab)}.
+     *
+     * @param tab The creative tab to use.
+     * @return The instance of this manager.
+     */
     public AlmostManager defaultCreativeTab(AlmostCreativeTab tab) {
         items.defaultCreativeTab(tab);
         tab.bindLang(dataGen);
         return this;
     }
 
+    /**
+     * Gets the namespace that is assigned to this manager.
+     *
+     * @return The namespace.
+     */
     public String getNamespace() {
         return namespace;
     }
 
+    /**
+     * Creates a new {@link ResourceLocation} for the given path within this manager's namespace.
+     *
+     * @param path The path to use.
+     * @return A new {@link ResourceLocation}.
+     */
     public ResourceLocation getRL(String path) {
         return new ResourceLocation(namespace, path);
     }
 
+    /**
+     * Gets the {@link ItemRegistration} for this manager.
+     *
+     * @return The {@link ItemRegistration}.
+     */
     public ItemRegistration items() {
         return items;
     }
 
+    /**
+     * Gets the {@link BlockRegistration} for this manager.
+     *
+     * @return The {@link BlockRegistration}.
+     */
     public BlockRegistration blocks() {
         return blocks;
     }
 
+    /**
+     * Gets the {@link BlockEntityRegistration} for this manager.
+     *
+     * @return The {@link BlockEntityRegistration}.
+     */
     public BlockEntityRegistration blockEntities() {
         return blockEntities;
     }
 
+    /**
+     * Gets the {@link DataGenManager} for this manager.
+     *
+     * @return The {@link DataGenManager}.
+     */
     public DataGenManager dataGen() {
         return dataGen;
     }
 
+    /**
+     * Initializes the registries held by this manager to the platform this method
+     * is called on.
+     * <br>
+     * This has to be called after all objects have been registered to the manager.
+     */
     public void initRegistriesToLoader() {
         AlmostLib.PLATFORM.initRegistrations(items, blocks, blockEntities);
     }
