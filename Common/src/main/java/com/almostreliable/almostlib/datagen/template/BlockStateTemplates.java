e055acda18a1314960fd3373752a333bf515046e
Relentless <relentless@rlnt.dev>
2023-01-20 13:27:46 +0100

    add horizontal facing blockstate template

--- Common/src/main/java/com/almostreliable/almostlib/datagen/template/BlockStateTemplates.java
+++ Common/src/main/java/com/almostreliable/almostlib/datagen/template/BlockStateTemplates.java
@@ -14,6 +14,7 @@ import net.minecraft.data.models.model.TexturedModel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
 import java.util.function.BiConsumer;
 
@@ -22,24 +23,34 @@ public class BlockStateTemplates {
 
     public static <B extends Block> BiConsumer<BlockEntry<B>, BlockStateProvider> simple() {
         return (e, provider) -> {
-            ResourceLocation simpleModel = TexturedModel.CUBE.create(e.get(), provider.getModelConsumer());
-            var simpleVariant = MultiVariantGenerator.multiVariant(e.get(), Variant.variant().with(VariantProperties.MODEL, simpleModel));
-            provider.addBlockState(simpleVariant);
+            ResourceLocation modelLocation = TexturedModel.CUBE.create(e.get(), provider.getModelConsumer());
+            var variant = MultiVariantGenerator.multiVariant(e.get(), Variant.variant().with(VariantProperties.MODEL, modelLocation));
+            provider.addBlockState(variant);
         };
     }
 
-    public static <B extends Block> BiConsumer<BlockEntry<B>, BlockStateProvider> withFacing() {
+    public static <B extends Block> BiConsumer<BlockEntry<B>, BlockStateProvider> facing() {
+        return facingState(BlockStateProperties.FACING, (modelLocation, p) ->
+            p.select(Direction.DOWN, Variant.variant().with(VariantProperties.MODEL, modelLocation)
+                    .with(VariantProperties.X_ROT, VariantProperties.Rotation.R90))
+                .select(Direction.UP, Variant.variant().with(VariantProperties.MODEL, modelLocation)
+                    .with(VariantProperties.X_ROT, VariantProperties.Rotation.R270)));
+    }
+
+    public static <B extends Block> BiConsumer<BlockEntry<B>, BlockStateProvider> horizontalFacing() {
+        return facingState(BlockStateProperties.HORIZONTAL_FACING, (modelLocation, p) -> {});
+    }
+
+    private static <B extends Block> BiConsumer<BlockEntry<B>, BlockStateProvider> facingState(
+        DirectionProperty prop, BiConsumer<ResourceLocation, PropertyDispatch.C1<Direction>> additional
+    ) {
         return (e, provider) -> {
             var mapping = new TextureMapping()
                 .put(TextureSlot.FRONT, TextureMapping.getBlockTexture(e.get(), "_front"))
                 .put(TextureSlot.SIDE, TextureMapping.getBlockTexture(e.get()))
                 .put(TextureSlot.TOP, TextureMapping.getBlockTexture(e.get()));
-            var modelLocation = ModelTemplates.CUBE_ORIENTABLE.create(e.get(), mapping, provider.getModelConsumer());
-            var facingProp = PropertyDispatch.property(BlockStateProperties.FACING)
-                .select(Direction.DOWN, Variant.variant().with(VariantProperties.MODEL, modelLocation)
-                    .with(VariantProperties.X_ROT, VariantProperties.Rotation.R90))
-                .select(Direction.UP, Variant.variant().with(VariantProperties.MODEL, modelLocation)
-                    .with(VariantProperties.X_ROT, VariantProperties.Rotation.R270))
+            ResourceLocation modelLocation = ModelTemplates.CUBE_ORIENTABLE.create(e.get(), mapping, provider.getModelConsumer());
+            var facingProp = PropertyDispatch.property(prop)
                 .select(Direction.NORTH, Variant.variant().with(VariantProperties.MODEL, modelLocation))
                 .select(Direction.SOUTH, Variant.variant().with(VariantProperties.MODEL, modelLocation)
                     .with(VariantProperties.Y_ROT, VariantProperties.Rotation.R180))
@@ -47,7 +58,8 @@ public class BlockStateTemplates {
                     .with(VariantProperties.Y_ROT, VariantProperties.Rotation.R270))
                 .select(Direction.EAST, Variant.variant().with(VariantProperties.MODEL, modelLocation)
                     .with(VariantProperties.Y_ROT, VariantProperties.Rotation.R90));
-            MultiVariantGenerator variant = MultiVariantGenerator.multiVariant(e.get()).with(facingProp);
+            additional.accept(modelLocation, facingProp);
+            var variant = MultiVariantGenerator.multiVariant(e.get()).with(facingProp);
             provider.addBlockState(variant);
         };
     }
