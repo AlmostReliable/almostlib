e9a94fc3a4115e8b8c45cc38f538afd6b84dbe50
LLytho <main@lytho.dev>
2023-02-24 12:29:13 +0100

    Remove test init from forge

--- Forge/src/main/java/com/almostreliable/almostlib/forge/ForgeInitializer.java
+++ Forge/src/main/java/com/almostreliable/almostlib/forge/ForgeInitializer.java
@@ -1,21 +1,11 @@
 package com.almostreliable.almostlib.forge;
 
 import com.almostreliable.almostlib.BuildConfig;
-import com.almostreliable.almostlib.TestEntry;
-import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BuildConfig.MOD_ID)
 public class ForgeInitializer {
 
     public ForgeInitializer() {
-        TestEntry.init();
-        TestEntry.MANAGER.initRegistriesToLoader();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onDataGen);
-    }
-
-    public void onDataGen(GatherDataEvent e) {
-        TestEntry.MANAGER.dataGen().collectProviders(e.getGenerator());
     }
 }
