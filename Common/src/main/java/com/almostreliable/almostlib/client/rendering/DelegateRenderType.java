49071f856cc2b9029a5c950b91551e445096d653
Relentless <relentless@rlnt.dev>
2023-03-12 20:58:48 +0100

    alpha buffer source and render type delegate (#6)
    Co-authored-by: LLytho <main@lytho.dev>

---
+++ Common/src/main/java/com/almostreliable/almostlib/client/rendering/DelegateRenderType.java
@@ -0,0 +1,48 @@
+package com.almostreliable.almostlib.client.rendering;
+
+import com.almostreliable.almostlib.BuildConfig;
+import com.almostreliable.almostlib.mixin.RenderTypeAccessor;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.RenderType;
+
+/**
+ * A {@link RenderType} that delegates to another {@link RenderType} and allows
+ * to run custom code before and after the delegate's setup and clear methods.
+ */
+@Environment(EnvType.CLIENT)
+final class DelegateRenderType extends RenderType {
+
+    private final RenderType delegate;
+
+    DelegateRenderType(RenderType delegate, Runnable setup, Runnable clear) {
+        super(
+            String.format("%s_delegate", BuildConfig.MOD_ID),
+            delegate.format(),
+            delegate.mode(),
+            delegate.bufferSize(),
+            delegate.affectsCrumbling(),
+            ((RenderTypeAccessor) delegate).getSortOnUpload(),
+            setup,
+            clear
+        );
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void setupRenderState() {
+        delegate.setupRenderState();
+        super.setupRenderState();
+    }
+
+    @Override
+    public void clearRenderState() {
+        delegate.clearRenderState();
+        super.clearRenderState();
+    }
+
+    @Override
+    public String toString() {
+        return "DelegateRenderType{delegate=" + delegate + "}";
+    }
+}
