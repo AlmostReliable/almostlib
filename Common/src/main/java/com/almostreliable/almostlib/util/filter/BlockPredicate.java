f069ac37b76e57a4139445321d1de58c4dc4a340
LLytho <main@lytho.dev>
2023-06-28 13:19:42 +0200

    Implement own property predicates

--- Common/src/main/java/com/almostreliable/almostlib/util/filter/BlockPredicate.java
+++ Common/src/main/java/com/almostreliable/almostlib/util/filter/BlockPredicate.java
@@ -5,7 +5,6 @@ import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
 import com.google.gson.*;
 import net.minecraft.advancements.critereon.NbtPredicate;
-import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
@@ -13,23 +12,25 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.TagKey;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.StateHolder;
 import net.minecraft.world.level.block.state.properties.Property;
 
 import javax.annotation.Nullable;
 import java.util.*;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class BlockPredicate implements Predicate<BlockState> {
 
-    public static final BlockPredicate ANY = new BlockPredicate(List.of(), Set.of(), StatePropertiesPredicate.ANY, NbtPredicate.ANY);
+    public static final BlockPredicate ANY = new BlockPredicate(List.of(), Set.of(), List.of(), NbtPredicate.ANY);
     public static final Serializer SERIALIZER = new Serializer();
-
     private final List<TagKey<Block>> tags;
     private final Set<Block> blocks;
-    private final StatePropertiesPredicate properties;
+    private final List<PropertyPredicate> properties;
     private final NbtPredicate nbt;
     @Nullable Set<Block> decomposedBlocks;
     @Nullable Multimap<Block, BlockState> decomposedBlockStates;
@@ -43,34 +44,45 @@ public class BlockPredicate implements Predicate<BlockState> {
         return new Builder().tags(tags);
     }
 
-    private BlockPredicate(List<TagKey<Block>> tags, Set<Block> blocks, StatePropertiesPredicate properties, NbtPredicate nbt) {
+    private BlockPredicate(List<TagKey<Block>> tags, Set<Block> blocks, List<PropertyPredicate> properties, NbtPredicate nbt) {
         this.tags = tags;
         this.blocks = blocks;
         this.properties = properties;
         this.nbt = nbt;
     }
 
-    private boolean testTags(BlockState state) {
+    public boolean testTags(BlockState state) {
         for (TagKey<Block> tag : tags) {
             if (state.is(tag)) return true;
         }
         return false;
     }
 
-    private boolean testBlocks(BlockState state) {
+    public boolean testBlocks(BlockState state) {
         return blocks.contains(state.getBlock());
     }
 
-    private boolean testNbt(ServerLevel level, BlockPos pos) {
+    public boolean testNbt(ServerLevel level, BlockPos pos) {
         if (nbt == NbtPredicate.ANY) return true;
         var blockEntity = level.getBlockEntity(pos);
         return blockEntity != null && nbt.matches(blockEntity.saveWithFullMetadata());
     }
 
+    public boolean testProperties(BlockState state) {
+        var stateDefinition = state.getBlock().getStateDefinition();
+        for (PropertyPredicate property : properties) {
+            if (!property.match(stateDefinition, state)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     @Override
     public boolean test(BlockState state) {
         if (equals(ANY)) return true;
-        return (testTags(state) || testBlocks(state)) && properties.matches(state);
+        return (testTags(state) || testBlocks(state)) && testProperties(state);
     }
 
     public boolean test(ServerLevel level, BlockPos pos) {
@@ -115,7 +127,7 @@ public class BlockPredicate implements Predicate<BlockState> {
         ImmutableMultimap.Builder<Block, BlockState> builder = ImmutableMultimap.builder();
         for (Block block : decomposeBlocks()) {
             for (BlockState state : block.getStateDefinition().getPossibleStates()) {
-                if (properties.matches(state)) {
+                if (testProperties(state)) {
                     builder.put(block, state);
                 }
             }
@@ -128,25 +140,20 @@ public class BlockPredicate implements Predicate<BlockState> {
     /**
      * Returns a list of {@link Difference}'s between the predicate and the given block state.
      *
-     * @param state The block state to compare against.
-     * @return A list of differences.
+     * @param state  The block state to compare against.
+     * @param onDiff A callback that will be called for each property.
      */
-    public List<Difference> difference(BlockState state) {
-        List<Difference> differences = new ArrayList<>();
+    public void difference(BlockState state, BiConsumer<PropertyPredicate, Difference> onDiff) {
         var stateDefinition = state.getBlock().getStateDefinition();
-        for (var matcher : properties.properties) {
-            Property<?> property = stateDefinition.getProperty(matcher.getName());
-            boolean matched = matcher.match(stateDefinition, state);
-            if (matcher instanceof StatePropertiesPredicate.ExactPropertyMatcher epm) {
-                var diff = new ExactDifference(epm.getName(), matched, property, epm.value);
-                differences.add(diff);
-            } else if (matcher instanceof StatePropertiesPredicate.RangedPropertyMatcher rpm) {
-                var diff = new RangedDifference(rpm.getName(), matched, property, rpm.minValue, rpm.maxValue);
-                differences.add(diff);
+        for (PropertyPredicate pp : properties) {
+            Property<?> property = stateDefinition.getProperty(pp.getName());
+            if (property == null) {
+                onDiff.accept(pp, new MissingPropertyDifference(pp.getName()));
+            } else {
+                boolean matches = pp.match(stateDefinition, state);
+                onDiff.accept(pp, pp.difference(matches, property));
             }
         }
-
-        return differences;
     }
 
     @SuppressWarnings("UnusedReturnValue")
@@ -154,7 +161,7 @@ public class BlockPredicate implements Predicate<BlockState> {
 
         private final List<TagKey<Block>> tags = new ArrayList<>();
         private final Set<Block> blocks = new HashSet<>();
-        private StatePropertiesPredicate properties = StatePropertiesPredicate.ANY;
+        private final List<PropertyPredicate> properties = new ArrayList<>();
         private NbtPredicate nbt = NbtPredicate.ANY;
 
         private Builder() {
@@ -187,15 +194,42 @@ public class BlockPredicate implements Predicate<BlockState> {
             return this;
         }
 
-        public Builder properties(StatePropertiesPredicate spp) {
-            this.properties = spp;
+        public Builder hasProperty(Property<?> property, String value) {
+            this.properties.add(new ExactPropertyPredicate(property.getName(), value));
             return this;
         }
 
-        public Builder properties(Consumer<StatePropertiesPredicate.Builder> consumer) {
-            var builder = StatePropertiesPredicate.Builder.properties();
-            consumer.accept(builder);
-            this.properties = builder.build();
+        public Builder hasProperty(Property<Integer> property, int value) {
+            return hasProperty(property, Integer.toString(value));
+        }
+
+        public Builder hasProperty(Property<Boolean> property, boolean value) {
+            return hasProperty(property, Boolean.toString(value));
+        }
+
+        public <T extends Comparable<T> & StringRepresentable> Builder hasProperty(Property<T> property, T value) {
+            return this.hasProperty(property, value.getSerializedName());
+        }
+
+        public Builder hasProperty(Property<?> property, @Nullable String min, @Nullable String max) {
+            this.properties.add(new RangedPropertyPredicate(property.getName(), min, max));
+            return this;
+        }
+
+        public Builder hasProperty(Property<Integer> property, @Nullable Integer min, @Nullable Integer max) {
+            String minStr = min == null ? null : Integer.toString(min);
+            String maxStr = max == null ? null : Integer.toString(max);
+            return hasProperty(property, minStr, maxStr);
+        }
+
+        public <T extends Comparable<T> & StringRepresentable> Builder hasProperty(Property<T> property, @Nullable T min, @Nullable T max) {
+            String minStr = min == null ? null : min.getSerializedName();
+            String maxStr = max == null ? null : max.getSerializedName();
+            return this.hasProperty(property, minStr, maxStr);
+        }
+
+        public Builder hasProperty(PropertyPredicate predicate) {
+            this.properties.add(predicate);
             return this;
         }
 
@@ -246,8 +280,13 @@ public class BlockPredicate implements Predicate<BlockState> {
                 json.add("nbt", blockPredicate.nbt.serializeToJson());
             }
 
-            if (blockPredicate.properties != StatePropertiesPredicate.ANY) {
-                json.add("state", blockPredicate.properties.serializeToJson());
+            if (!blockPredicate.properties.isEmpty()) {
+                JsonObject stateJson = new JsonObject();
+                for (PropertyPredicate property : blockPredicate.properties) {
+                    stateJson.add(property.getName(), property.toJson());
+                }
+
+                json.add("state", stateJson);
             }
 
             return json;
@@ -279,7 +318,7 @@ public class BlockPredicate implements Predicate<BlockState> {
             }
 
             if (json.get("state") instanceof JsonObject properties) {
-                builder.properties(StatePropertiesPredicate.fromJson(properties));
+                readStatePredicates(builder, properties);
             }
 
             return builder.build();
@@ -320,6 +359,104 @@ public class BlockPredicate implements Predicate<BlockState> {
 
             return new JsonArray();
         }
+
+        private static void readStatePredicates(Builder builder, JsonObject properties) {
+            for (var e : properties.entrySet()) {
+                if (e.getValue() instanceof JsonPrimitive exact) {
+                    builder.hasProperty(new ExactPropertyPredicate(e.getKey(), exact.getAsString()));
+                }
+
+                if (e.getValue() instanceof JsonObject ranged) {
+                    String min = getStringOrNull(ranged.get("min"));
+                    String max = getStringOrNull(ranged.get("max"));
+                    builder.hasProperty(new RangedPropertyPredicate(e.getKey(), min, max));
+                }
+            }
+        }
+
+        @Nullable
+        private static String getStringOrNull(@Nullable JsonElement json) {
+            if (json == null) {
+                return null;
+            }
+
+            return json.isJsonNull() ? null : json.getAsString();
+        }
+    }
+
+    public interface PropertyPredicate {
+
+        default <S extends StateHolder<?, S>> boolean match(StateDefinition<?, S> properties, BlockState state) {
+            Property<?> property = properties.getProperty(getName());
+            return property != null && this.match(state, property);
+        }
+
+        <T extends Comparable<T>> boolean match(BlockState state, Property<T> property);
+
+        Difference difference(boolean matches, Property<?> property);
+
+        String getName();
+
+        JsonElement toJson();
+    }
+
+    public record ExactPropertyPredicate(String getName, String value) implements PropertyPredicate {
+
+        @Override
+        public <T extends Comparable<T>> boolean match(BlockState state, Property<T> property) {
+            T comparable = state.getValue(property);
+            return property.getValue(value).map(v -> comparable.compareTo(v) == 0).orElse(false);
+        }
+
+        @Override
+        public Difference difference(boolean matches, Property<?> property) {
+            return new ExactDifference(getName(), matches, property, value);
+        }
+
+        @Override
+        public JsonElement toJson() {
+            return new JsonPrimitive(value);
+        }
+    }
+
+    public record RangedPropertyPredicate(String getName, @Nullable String min, @Nullable String max) implements PropertyPredicate {
+
+        @Override
+        public <T extends Comparable<T>> boolean match(BlockState state, Property<T> property) {
+            T comparable = state.getValue(property);
+            if (min != null) {
+                Optional<T> minOpt = property.getValue(min);
+                boolean matches = minOpt.map(v -> comparable.compareTo(v) < 0).orElse(false);
+                if (!matches) return false;
+            }
+
+            if (max != null) {
+                Optional<T> maxOpt = property.getValue(max);
+                boolean matches = maxOpt.map(v -> comparable.compareTo(v) > 0).orElse(false);
+                if (!matches) return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public Difference difference(boolean matches, Property<?> property) {
+            return new RangedDifference(getName(), matches, property, min, max);
+        }
+
+        @Override
+        public JsonElement toJson() {
+            JsonObject json = new JsonObject();
+            if (this.min != null) {
+                json.addProperty("min", this.min);
+            }
+
+            if (this.max != null) {
+                json.addProperty("max", this.max);
+            }
+
+            return json;
+        }
     }
 
     public interface Difference {
@@ -342,6 +479,25 @@ public class BlockPredicate implements Predicate<BlockState> {
         }
 
         String getValueRepresentation();
+    }
+
+    record MissingPropertyDifference(String getName) implements Difference {
+
+        @Override
+        public boolean isMatch() {
+            return false;
+        }
+
+        @Nullable
+        @Override
+        public Property<?> getProperty() {
+            return null;
+        }
+
+        @Override
+        public String getValueRepresentation() {
+            return "not exist";
+        }
     }
 
     record ExactDifference(String getName, boolean isMatch, @Nullable Property<?> getProperty, String getValue) implements Difference {
