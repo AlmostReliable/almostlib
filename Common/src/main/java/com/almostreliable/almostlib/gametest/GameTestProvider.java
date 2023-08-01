6e1032354ffeb0f66ee777b94a5b7917caae0afa
Relentless <relentless@rlnt.dev>
2023-08-01 14:21:33 +0200

    add todo for custom game test helper

--- Common/src/main/java/com/almostreliable/almostlib/gametest/GameTestProvider.java
+++ Common/src/main/java/com/almostreliable/almostlib/gametest/GameTestProvider.java
@@ -1,5 +1,5 @@
 package com.almostreliable.almostlib.gametest;
 
 public interface GameTestProvider {
-
+    // TODO: add custom GameTestHelper with utility function for `getBlockEntity(BlockPos, Class)` that tests the BE automatically
 }
