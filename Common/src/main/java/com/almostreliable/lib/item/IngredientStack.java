4cca47466efe1b68a6b5f3350872457988932552
Relentless <relentless@rlnt.dev>
2023-08-30 22:15:13 +0200

    use serializer instance for IngredientStack

--- Common/src/main/java/com/almostreliable/lib/item/IngredientStack.java
+++ Common/src/main/java/com/almostreliable/lib/item/IngredientStack.java
@@ -7,6 +7,8 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import javax.annotation.Nullable;
+
 /**
  * A class representing an ingredient with a count.
  * <p>
@@ -20,6 +22,8 @@ import net.minecraft.world.item.crafting.Ingredient;
  * The deserializer is also capable of reading vanilla ingredients.
  */
 public class IngredientStack {
+
+    public static final Serializer SERIALIZER = new Serializer();
 
     protected final Ingredient ingredient;
     protected final int count;
@@ -48,77 +52,79 @@ public class IngredientStack {
         return new IngredientStack(ingredient, 1);
     }
 
-    /**
-     * Deserializes an IngredientStack from the given {@link JsonElement}.
-     * <p>
-     * Capable of reading vanilla ingredients that are serialized as {@link JsonObject}
-     * and custom ingredients that are serialized as {@link JsonArray}.
-     *
-     * @param json The json element.
-     * @return The IngredientStack.
-     */
-    public static IngredientStack fromJson(JsonElement json) {
-        if (json instanceof JsonObject jsonObject && jsonObject.has("ingredient")) {
-            // nested ingredient found, serialized IngredientStack
-            Ingredient ingredient = Ingredient.fromJson(jsonObject.get("ingredient"));
-            int count = GsonHelper.getAsInt(jsonObject, "count", 1);
-            return of(ingredient, count);
-        }
-
-        if (json instanceof JsonObject || json instanceof JsonArray) {
-            // vanilla ingredient found, serialized Ingredient
-            return of(Ingredient.fromJson(json));
-        }
-
-        throw new IllegalArgumentException("Expected either a JsonObject or a JsonArray");
-    }
-
-    /**
-     * Deserializes an IngredientStack from the given {@link FriendlyByteBuf}.
-     *
-     * @param buffer The buffer.
-     * @return The IngredientStack.
-     */
-    public static IngredientStack fromNetwork(FriendlyByteBuf buffer) {
-        var ingredient = Ingredient.fromNetwork(buffer);
-        var count = buffer.readVarInt();
-        return new IngredientStack(ingredient, count);
-    }
-
-    /**
-     * Serializes this IngredientStack to a {@link JsonObject}.
-     * <p>
-     * Omits the "count" property if the count is 1.
-     *
-     * @return The json object.
-     */
-    public JsonObject toJson() {
-        var json = new JsonObject();
-        json.add("ingredient", ingredient.toJson());
-        if (count > 1) {
-            json.addProperty("count", count);
-        }
-        return json;
-    }
-
-    /**
-     * Serializes this IngredientStack to the given {@link FriendlyByteBuf}.
-     * <p>
-     * The ingredient is serialized using {@link Ingredient#toNetwork(FriendlyByteBuf)}.<br>
-     * The count is always serialized as a var int.
-     *
-     * @param buffer The buffer to write to.
-     */
-    public void toNetwork(FriendlyByteBuf buffer) {
-        ingredient.toNetwork(buffer);
-        buffer.writeVarInt(count);
-    }
-
     public Ingredient getIngredient() {
         return ingredient;
     }
 
     public int getCount() {
         return count;
+    }
+
+    public static class Serializer {
+
+        /**
+         * Deserializes the given {@link IngredientStack} from the given {@link JsonElement}.
+         * <p>
+         * Capable of reading vanilla ingredients that are serialized as {@link JsonObject}
+         * and custom ingredients that are serialized as {@link JsonArray}.
+         *
+         * @param json The json element to deserialize.
+         * @return The IngredientStack.
+         */
+        public static IngredientStack fromJson(@Nullable JsonElement json) {
+            if (json instanceof JsonObject jsonObject && jsonObject.has("ingredient")) {
+                // nested ingredient found, serialized IngredientStack
+                Ingredient ingredient = Ingredient.fromJson(jsonObject.get("ingredient"));
+                int count = GsonHelper.getAsInt(jsonObject, "count", 1);
+                return of(ingredient, count);
+            }
+
+            if (json instanceof JsonObject || json instanceof JsonArray) {
+                // vanilla ingredient found, serialized Ingredient
+                return of(Ingredient.fromJson(json));
+            }
+
+            throw new IllegalArgumentException("Expected either a JsonObject or a JsonArray");
+        }
+
+        /**
+         * Deserializes the given {@link IngredientStack} from the given {@link FriendlyByteBuf}.
+         *
+         * @param buffer The buffer to read from.
+         * @return The IngredientStack.
+         */
+        public static IngredientStack fromNetwork(FriendlyByteBuf buffer) {
+            var ingredient = Ingredient.fromNetwork(buffer);
+            var count = buffer.readVarInt();
+            return new IngredientStack(ingredient, count);
+        }
+
+        /**
+         * Serializes the given {@link IngredientStack} to a {@link JsonObject}.
+         * <p>
+         * Omits the "count" property if the count is 1.
+         *
+         * @param ingredientStack The ingredient stack to serialize.
+         * @return The json object.
+         */
+        public JsonObject toJson(IngredientStack ingredientStack) {
+            var json = new JsonObject();
+            json.add("ingredient", ingredientStack.ingredient.toJson());
+            if (ingredientStack.count > 1) {
+                json.addProperty("count", ingredientStack.count);
+            }
+            return json;
+        }
+
+        /**
+         * Serializes the given {@link IngredientStack} to the given {@link FriendlyByteBuf}.
+         *
+         * @param buffer The buffer to write to.
+         * @param ingredientStack The ingredient stack to serialize.
+         */
+        public void toNetwork(FriendlyByteBuf buffer, IngredientStack ingredientStack) {
+            ingredientStack.ingredient.toNetwork(buffer);
+            buffer.writeVarInt(ingredientStack.count);
+        }
     }
 }
